a8162074a3b86a9a1af42437e4098411311587cb
JoshuaZecha <31937496+JoshuaZecha@users.noreply.github.com>
2017-12-23 20:04:41 +0100

    Add files via upload

--- Accessing Game History.R
+++ Accessing Game History.R
@@ -13,11 +13,10 @@ library(stringr)
 # Regional codes:
 # https://developer.riotgames.com/regional-endpoints.html
 
-#####1.##### FOR EUW
+#####1.##### 
+# This instruction is FOR EUW
+#Replace the <text> with own values
 # Input: Summoner name + API Key
-
-#Replace values inside <> with own values
-
 SUMMONERNAME <- as.data.frame("<Summonername>")
 names(SUMMONERNAME)[1]<- "summonername"
 SUMMONERNAME$summonername<-as.character(SUMMONERNAME$summonername)
@@ -56,21 +55,9 @@ jsonrecentgames<-as.data.frame(jsonrecentgames)
 ###Extracting a Champion ID (ID)in this case Teemo or LULU)#######
 
 ###this may depend on patch:
-allchampions <- fromJSON("http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json")
+allchampions <- fromJSON("http://ddragon.leagueoflegends.com/cdn/7.24.2/data/en_US/champion.json")
 allchampions<-as.data.frame(allchampions)
 lulu_id<-allchampions%>%select(data.Lulu.key)%>%filter( row_number() == 1L)
 teemo_id<-allchampions%>%select(data.Teemo.key)%>%filter( row_number() == 1L)
 teemo_id$data.Teemo.key<-as.character(teemo_id$data.Teemo.key)
 
-#####Mastery########## -> Not reliable working yet
-baselinkforjsonmastery<-as.data.frame(c("https://euw1.api.riotgames.com//lol/champion-mastery/v3/champion-masteries/by-summoner/SUMMONERID/by-champion/CHAMPID?api_key=APIKEY"))
-names(baselinkforjsonmastery)[1]<- "link"
-
-baselinkforjsonmastery$link<-as.character(baselinkforjsonmastery$link)
-baselinkforjsonmastery$full_link_recentML <- str_replace_all(baselinkforjsonmastery$link, "SUMMONERID", summonerID$accountId)
-baselinkforjsonmastery$full_link_recentML<- as.character(baselinkforjsonmastery$full_link_recentML)
-baselinkforjsonmastery$full_link_recentML <- str_replace_all(baselinkforjsonmastery$full_link_recentML, "APIKEY", APIKEY$key)
-baselinkforjsonmastery$full_link_recentML<- as.character(baselinkforjsonmastery$full_link_recentML)
-baselinkforjsonmastery$full_link_recentML <- str_replace_all(baselinkforjsonmastery$full_link_recentML, "CHAMPID", teemo_id$data.Teemo.key)
-mastery <- fromJSON(baselinkforjsonmastery$full_link_recentML)
-mastery<-as.data.frame(mastery)
